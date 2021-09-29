0b14247dfaeefba32dd08c1c3aef8dee29e570d2
Lorenzo Hermoso Moreno <lmhm.yeah@gmail.com>
2021-09-29 03:52:29 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { CssBaseline } from '@mui/material';
-import { Footer } from "../components/Footer";
-import { Navbar } from "../components/Navbar";
+import { Footer } from "../src/components/Footer";
+import { Navbar } from "../src/components/Navbar";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
